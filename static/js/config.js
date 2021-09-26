06c491aa9f58662826bd5d0ddf1ec5adc545898c
alnrom <34905669+alnrom@users.noreply.github.com>
2021-09-26 11:31:13 -0500

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY = "pk.eyJ1IjoiYWxuLXJvbSIsImEiOiJja29jM29tcTcyYjE5MnZydzdldnNveXphIn0.W8Cxmz85LZ0tykcmJKBDWg"
---
+++ static/js/map.js
@@ -0,0 +1,16 @@
+var myMap = L.map("map", {
+    center: [25.74607192117577, -100.26948426658805],
+    zoom: 15
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+  L.marker([25.746145739399118, -100.26947158323435]).addTo(myMap);

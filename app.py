5c20a541597cb0692e86788635905018c98807ca
alanrom <romeroa@MacBook-Air-de-Jesus.local>
2021-09-30 12:47:30 -0500

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return the dashboard homepage"""
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
